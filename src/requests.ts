0bb78e647bc5df924f4cad1d4896bf65df7a1926
Cофья Гергет <146007213+sslonixxx@users.noreply.github.com>
2024-10-20 23:54:54 +0700

    done

--- src/requests.ts
+++ src/requests.ts
@@ -1,27 +1,15 @@
 import axios from "axios";
 import { FormSchema } from "./validationSchema";
 
-export const postUsersSignin = async (data:FormSchema) => {
-    try {
-      await axios.post('https://shift-backend.onrender.com/users/signin', data, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  };
+export const postUsersSignin = async (data:FormSchema) => 
+  axios.post('https://shift-backend.onrender.com/users/signin', data, {
+    headers: {
+      'Content-Type': 'application/json',
+    }})
   
 
-export const handleSendRequest = async (data: FormSchema) => {
-  try {
-    await axios.post('https://shift-backend.onrender.com/auth/otp', data, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
+export const handleSendRequest = async (data: FormSchema) => 
+  axios.post('https://shift-backend.onrender.com/auth/otp', data, {
+    headers: {
+      'Content-Type': 'application/json',
+    }})
--- src/validationSchema.ts
+++ src/validationSchema.ts
@@ -4,13 +4,15 @@ export const formSchema = z
 .object({
   phone: z
   .string()
-  .min(1, {message: 'Поле является обязательным'}),
+  .min(1, {message: 'Поле является обязательным'})
+  .length(11, {message: 'Телефон должен состоять из 11 цифр'})
 })
 
 export const formWithOTP = z.object({
   phone: z
   .string()
-  .min(1, {message: 'Поле является обязательным'}),
+  .min(1, {message: 'Поле является обязательным'})
+  .length(11, {message: 'Телефон должен состоять из 11 цифр'}),
   code: z
   .string()
   .length(6, {message:'Код должен содержать 6 цифр'})
